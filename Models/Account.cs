152381b60f0e760680b17618234e76f9dd5b7efe
toanpv224 <toanpv224@gmail.com>
2022-07-20 13:42:05 +0700

    Create Account.cs

---
+++ Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ProjectPRN211.Models
+{
+    public partial class Account
+    {
+        public Account()
+        {
+            Comments = new HashSet<Comment>();
+            Posts = new HashSet<Post>();
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Displayname { get; set; }
+
+        public virtual ICollection<Comment> Comments { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
